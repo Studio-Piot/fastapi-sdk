18c01b8573400e2df3efe437c2f2a52aedbbe10b
Guillaume Piot <guillaume@piot.co.uk>
2025-11-24 17:37:39 +0000

    Update default key path

--- tests/config.py
+++ tests/config.py
@@ -16,10 +16,10 @@ class Settings(BaseSettings):
     MONGO_DATABASE_NAME: str = os.environ.get("MONGO_DATABASE_NAME", "test_fastapi_sdk")
     PUBLIC_ROUTES: list[str] = json.loads(os.environ.get("PUBLIC_ROUTES", "[]"))
     TEST_PRIVATE_KEY_PATH: str = os.environ.get(
-        "TEST_PRIVATE_KEY_PATH", "test_private_key.pem"
+        "TEST_PRIVATE_KEY_PATH", "tests/test_private_key.pem"
     )
     TEST_PUBLIC_KEY_PATH: str = os.environ.get(
-        "TEST_PUBLIC_KEY_PATH", "test_public_key.pem"
+        "TEST_PUBLIC_KEY_PATH", "tests/test_public_key.pem"
     )
     AUTH_ISSUER: str = os.environ.get("AUTH_ISSUER", "https://auth.fauthy.com")
     AUTH_CLIENT_ID: str = os.environ.get("AUTH_CLIENT_ID", "test_client_id")
